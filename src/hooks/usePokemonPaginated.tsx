dff0151f4c05ef8e6df2bcddd9c6eb1ccde4292d
cedgo <cedgo1997@gmail.com>
2021-09-22 02:50:37 -0400

    retrieving pokemon data, interfaces created.

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -1,19 +1,27 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { pokemonApi } from '../api/pokemonApi';
+import { PokemonPaginatedResponse, Result, SimplePokemon } from '../interfaces/pokemonInterfaces';
 
 export const usePokemonPaginated = () => {
+
+    const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([]);
 
     const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40');
 
     const loadPokemons = async () => {
-        const resp = await pokemonApi.get(nextPageUrl.current);
-        console.log(JSON.stringify(resp.data, null, 2));
+        const resp = await pokemonApi.get<PokemonPaginatedResponse>(nextPageUrl.current);
+        nextPageUrl.current = resp.data.next;
+        mapPokemonList(resp.data.results);
+    }
+
+    const mapPokemonList = (pokemonList: Result[]) => {
+        pokemonList.forEach(pkmn => console.log(pkmn.name));
     }
 
     useEffect(() => {
         loadPokemons();
     }, []);
 
-    return {  };
+    return {};
 
 };
