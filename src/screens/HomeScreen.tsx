c208fb1446ec61fba812bcc68028c483074206e2
cedgo <cedgo1997@gmail.com>
2021-09-22 02:37:22 -0400

    Log pokemon info with axios and a custom hook

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Image, Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
 import { styles } from '../theme/appTheme';
 
 export const HomeScreen = () => {
 
     const { top } = useSafeAreaInsets();
-
+    usePokemonPaginated();
     return (
         <>
             <Image
                 source={require('./../assets/pokebola.png')}
                 style={styles.pokeballBG}
             />
+
             <Text style={{
                 ...styles.title,
                 ...styles.globalMargin,
