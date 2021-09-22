968e8dfffe7ecd57a8193ed512064f748f2e0cc9
cedgo <cedgo1997@gmail.com>
2021-09-22 01:58:20 -0400

    HomeScreen first look setup

--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -1,12 +1,20 @@
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from '../screens/HomeScreen';
+import { PokemonScreen } from '../screens/PokemonScreen';
 
 const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
     return (
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{
+            headerShown: false,
+            cardStyle: {
+                backgroundColor: 'white'
+            }
+        }}>
             <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Pokemon" component={PokemonScreen} />
         </Stack.Navigator>
     );
 }
