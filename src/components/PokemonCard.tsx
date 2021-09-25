39967e46b3ee4431c30569ac0ec08587a4919876
cedgo <cedgo1997@gmail.com>
2021-09-25 19:03:19 -0400

    Modifying pokemon card

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet, Dimensions, Image } from 'react-native';
 import { SimplePokemon } from '../interfaces/pokemonInterfaces';
+import { FadeInImage } from './FadeInImage';
 
 const { width } = Dimensions.get('window')
 
@@ -22,6 +23,18 @@ export const PokemonCard = ({ pokemon }: Props) => {
                     {'\n#' + pokemon.id}
                 </Text>
             </View>
+            <View style={styles.pokeballContainer}>
+                <Image
+                    source={require('../assets/pokebola-blanca.png')}
+                    style={styles.pokeball}
+                />
+            </View>
+            <FadeInImage
+                uri={pokemon.picture}
+                style={
+                    styles.pokemonImage
+                }
+            />
         </TouchableOpacity>
     );
 };
@@ -33,12 +46,35 @@ const styles = StyleSheet.create({
         height: 120,
         width: width * 0.4,
         marginBottom: 25,
-        borderRadius: 10
+        borderRadius: 10,
     },
     name: {
         color: 'white',
         fontSize: 20,
         top: 20,
         left: 10
+    },
+    pokeball: {
+        width: 100,
+        height: 100,
+        position: 'absolute',
+        top: 25,
+        right: -15,
+    },
+    pokemonImage: {
+        width: 120,
+        height: 120,
+        position: 'absolute',
+        right: 2,
+        bottom: 10
+    },
+    pokeballContainer: {
+        width: 100,
+        height: 100,
+        position: 'absolute',
+        bottom: 25,
+        right: 10,
+        opacity: 0.5,
+        overflow: 'hidden',
     }
 });
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ActivityIndicator, Image, Text } from 'react-native';
+import { ActivityIndicator, Image, Text, View } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { PokemonCard } from '../components/PokemonCard';
@@ -16,34 +16,41 @@ export const HomeScreen = () => {
                 source={require('./../assets/pokebola.png')}
                 style={styles.pokeballBG}
             />
-            <FlatList
-                data={simplePokemonList}
-                keyExtractor={(pokemon) => pokemon.id}
-                renderItem={({ item }) => (
-                    <PokemonCard pokemon={item} />
-                )}
-                showsVerticalScrollIndicator={false}
-                onEndReached={loadPokemons}
-                onEndReachedThreshold={0.4}
-                numColumns={2}
-                ListHeaderComponent={(
-                    <Text style={{
-                        ...styles.title,
-                        ...styles.globalMargin,
-                        top: top + 20,
-                        marginBottom: top + 20
-                    }}>
-                        Pokédex
-                    </Text>
-                )}
-                ListFooterComponent={
-                    <ActivityIndicator
-                        style={{ height: 100 }}
-                        size={20}
-                        color="grey"
-                    />
-                }
-            />
+            <View
+                style={{
+                    alignItems: 'center'
+                }}
+            >
+                <FlatList
+                    data={simplePokemonList}
+                    keyExtractor={(pokemon) => pokemon.id}
+                    renderItem={({ item }) => (
+                        <PokemonCard pokemon={item} />
+                    )}
+                    showsVerticalScrollIndicator={false}
+                    onEndReached={loadPokemons}
+                    onEndReachedThreshold={0.4}
+                    numColumns={2}
+                    ListHeaderComponent={(
+                        <Text style={{
+                            ...styles.title,
+                            ...styles.globalMargin,
+                            top: top + 20,
+                            marginBottom: top + 20,
+                            paddingBottom: 5
+                        }}>
+                            Pokédex
+                        </Text>
+                    )}
+                    ListFooterComponent={
+                        <ActivityIndicator
+                            style={{ height: 100 }}
+                            size={20}
+                            color="grey"
+                        />
+                    }
+                />
+            </View>
         </>
     );
 };
