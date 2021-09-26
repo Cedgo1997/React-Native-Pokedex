15948e49e20d86b83078d65f97bf62a69fc55244
cedgo <cedgo1997@gmail.com>
2021-09-26 17:36:59 -0400

    Render info and icons

--- src/components/PokemonDetails.tsx
+++ src/components/PokemonDetails.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { FlatList, ScrollView } from 'react-native-gesture-handler';
 import { PokemonFull } from '../interfaces/pokemonInterfaces';
+import { FadeInImage } from './FadeInImage';
 
 interface Props {
     pokemon: PokemonFull;
@@ -38,13 +39,78 @@ export const PokemonDetails = ({ pokemon }: Props) => {
                         })
                     }
                 </View>
-                
+                {/* Weight */}
+                <Text style={styles.title}>
+                    Weight
+                </Text>
+                <Text style={styles.regularText}>
+                    {pokemon.weight / 10} kg
+                </Text>
+
                 {/* Sprites */}
-                <View style={{
-                    ...styles.container,
-                    marginTop: 20
-                }}>
+                <View>
                     <Text style={styles.title}>Sprites</Text>
+                </View>
+
+                <ScrollView
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                >
+                    <FadeInImage
+                        uri={pokemon.sprites.front_default}
+                        style={styles.basicSprite}
+                    />
+                    <FadeInImage
+                        uri={pokemon.sprites.back_default}
+                        style={styles.basicSprite}
+                    />
+                    <FadeInImage
+                        uri={pokemon.sprites.front_shiny}
+                        style={styles.basicSprite}
+                    />
+                    <FadeInImage
+                        uri={pokemon.sprites.back_shiny}
+                        style={styles.basicSprite}
+                    />
+                </ScrollView>
+
+                {/* Abilities */}
+                <View>
+                    <Text style={styles.title}>
+                        Abilities
+                    </Text>
+                    <View style={{ flexDirection: 'row' }}>
+                        {
+                            pokemon.abilities.map(({ ability }) => {
+                                return (
+                                    <Text
+                                        style={{ ...styles.regularText, marginRight: 10 }}
+                                        key={ability.name}
+                                    >
+                                        {ability.name}
+                                    </Text>)
+                            })
+                        }
+                    </View>
+                </View>
+                {/* Moves */}
+                <View>
+                    <Text style={styles.title}>
+                        Moves
+                    </Text>
+                    <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+                        {
+                            pokemon.moves.map(({ move }) => {
+                                return (
+                                    <Text
+                                        style={{ ...styles.regularText, marginRight: 10 }}
+                                        key={move.name}
+                                    >
+                                        {move.name}
+                                    </Text>)
+                            })
+                        }
+                    </View>
                 </View>
             </View>
         </ScrollView>
@@ -57,9 +123,14 @@ const styles = StyleSheet.create({
     },
     title: {
         fontSize: 22,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginTop: 20,
     },
     regularText: {
         fontSize: 19
+    },
+    basicSprite: {
+        width: 125,
+        height: 125,
     }
 });
